d028edd5b40c36a45a3e5f8e7eb7f05889875608
NEUmmc <bmc.bob@qq.com>
2020-05-27 22:08:22 +0800

    为了安全上传时省去密码

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: '239274',
+        password: '',//为了安全上传时省去密码
         port: '3306',
         database: 'data'
     }
